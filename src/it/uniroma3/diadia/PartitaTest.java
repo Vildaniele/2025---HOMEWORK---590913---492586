bb022fbc2b84ac27af246af4715cefb74f180968
Vildaniele <lele.villa19@gmail.com>
2025-03-25 16:56:16 +0100

    Add files via upload

---
+++ src/it/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,38 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+
+public class PartitaTest {
+
+	private Partita partita;
+	
+	@Before
+	public void setUp() {
+		this.partita = new Partita();
+	}
+	
+	@Test
+	public void testGetStanzaVincenteNotNull() {
+		assertNotNull(this.partita.getStanzaVincente());
+	}
+	
+	@Test
+	public void testVintaSeStanzaCorrenteEVincente() {
+		Stanza nonVincente = new Stanza("Non vincente");
+		this.partita.setStanzaCorrente(nonVincente);
+		assertFalse(this.partita.vinta());
+	}
+	
+	@Test
+	public void testFinitaSeVinta() {
+		this.partita.setStanzaCorrente(this.partita.getStanzaVincente());
+		assertTrue(this.partita.isFinita());
+	}
+	
+
+}
---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,30 @@
+package it.uniroma3.diadia.giocatore;
+
+public class Giocatore {
+	
+	static final public int CFU_INIZIALI = 20;
+	
+    /*Ogni giocatore ha la sua borsa*/
+	private Borsa borsa;
+	private int cfu;
+	
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+	
+	public Borsa getBorsa() {
+		return borsa;
+	}
+	public void setBorsa(Borsa borsa) {
+		this.borsa = borsa;
+	}
+	public int getCfu() {
+		return cfu;
+	}
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+	
+	
+}
